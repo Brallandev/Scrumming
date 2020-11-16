1be0db3d1d2d68b0bd5b8143f26ad47728197421
Kevin Gonzalez <kevinninog@usantotomas.edu.co>
2020-11-15 19:29:44 -0500

    Creacion de (Crear Proyectos)

--- crearproyecto.py
+++ crearproyecto.py
@@ -1,37 +1,38 @@
-<<<<<<< HEAD
 import psycopg2
 from  conexion import dc
-
-
-
 conexion=psycopg2.connect(**dc)
 
-Cursor = conexion.cursor()
+def crear (nombre,descripcion):
+    cursor = conexion.cursor()
 
-Cursor.execute('select * from Proyectos ')
+    sql = 'insert into  Proyectos(nombre, descripcion) values(%s, %s)'
 
-filas= Cursor.fetchall()
+    parametros = (nombre,descripcion)
 
-for fila in filas:
-    id=fila[0]
-    nombre= fila[1]
-    print(f'id={id}\tnombre={nombre}')
-Cursor.close()
-conexion.close()
-=======
-print("*******  Este es el inicio para la creacion de un nuevo Proyecto  **********")
-print("1. Crear un nuevo Proyecto ")
-print("2. Salir ")
-opcion= int(input("Digite la opcion deseada: "))
-if opcion==1:
-    print("Que nombre de proyecto desea")
-    nombre_proyecto=input()
-    print(f"El nombre del proyecto es : {nombre_proyecto}")
-    print("Desea agregar descripcion de su proyecto")
-    descripcion_proyecto=input()
-    print(f"Su descripcion es : {descripcion_proyecto}")
-else:
-     print("Usted ha decidido salir de creacion de proyecto")   
+    cursor.execute(sql, parametros)
+
+    conexion.commit()
+
+    cursor.close()
+
    
-print("end")
->>>>>>> b5d3002e7f9c75ffc80201537638f146c3f0d24f
+    
+def consultar():
+
+
+    Cursor = conexion.cursor()
+
+    Cursor.execute('select * from Proyectos ')
+
+    filas= Cursor.fetchall()
+
+    for fila in filas:
+        id=fila[0]
+        nombre= fila[1]
+        descripcion=fila[2]
+        print(f'id={id}\tnombre={nombre}\t descripcion={descripcion}')
+    Cursor.close()
+    conexion.close()
+
+crear ("prueba dos","prueba02")
+consultar()
