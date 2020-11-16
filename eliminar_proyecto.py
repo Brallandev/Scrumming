740b914bff8398ee940f8da5a3e28b0cd1a786b9
julian cuervo <julian.cuervo@usantotomas.edu.co>
2020-11-16 02:23:55 -0500

    creacion archivo para eliminacion de proyectos

---
+++ eliminar_proyecto.py
@@ -0,0 +1,19 @@
+import psycopg2
+from  conexion import dc
+is_connection= True
+try:
+     conexion=psycopg2.connect(**dc)
+
+except :
+    print("No se pudo conectar a la base de datos")
+    is_connection=False
+    input("\n"+"Pulse una tecla para Salir")
+    exit()
+def eliminar(id):
+    cursor = conexion.cursor()
+    sql = 'delete from proyectos where id=%s'
+    parametros = (id)
+    cursor.execute(sql, parametros)
+    conexion.commit()
+    cursor.close()
+conexion.close()
